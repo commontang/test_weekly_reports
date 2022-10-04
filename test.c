fa56e2831abc04d233608ec4b365d7aabb0a5284
commontang <114840596+commontang@users.noreply.github.com>
2022-10-04 17:16:06 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello,here is a progam with errors!~\n");
+    
+    /*
+      A sample multi-line comment sample
+      what error happened here
+    */
+   //Here is a single line comment
+
+   printf("My class is \"EIC202009\"\n");
+   printf("My name is \"Zhang sansan\"\n\a");
+
+   return 0;
+}
